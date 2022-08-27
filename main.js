629dc6e218a224da7d0003f7efe80e58659a790a
Izzy <izzycalabig@hotmail.com>
2022-08-26 17:12:35 -0700

    Tried clickable trees

--- main.js
+++ main.js
@@ -3,38 +3,76 @@ document.getElementById('treeTwo').addEventListener('click', hideTreeTwo)
 document.getElementById('treeThree').addEventListener('click', hideTreeThree)
 document.getElementById('treeFour').addEventListener('click', hideTreeFour)
 document.getElementById('treeFive').addEventListener('click', hideTreeFive)
+document.getElementById('treeSix').addEventListener('click', hideTreeSix)
+document.getElementById('treeSeven').addEventListener('click', hideTreeSeven)
+document.getElementById('treeEight').addEventListener('click', hideTreeEight)
+document.getElementById('treeNine').addEventListener('click', hideTreeNine)
+document.getElementById('treeTen').addEventListener('click', hideTreeTen)
+document.getElementById('treeEleven').addEventListener('click', hideTreeEleven)
+document.getElementById('treeTwelve').addEventListener('click', hideTreeTwelve)
+document.getElementById('treeThirteen').addEventListener('click', hideTreeThirteen)
+document.getElementById('treeFourteen').addEventListener('click', hideTreeFourteen)
+document.getElementById('treeFifteen').addEventListener('click', hideTreeFifteen)
 
 // Separated by individual trees
 
 function hideTreeOne() {
-    console.log('clicked')
     document.getElementById('treeOne').style.display = 'none'
 }
 
-document.getElementById('treeTwo').addEventListener('click', hideTree)
-
 function hideTreeTwo() {
-    console.log('clicked')
     document.getElementById('treeTwo').style.display = 'none'
 }
 
-document.getElementById('treeThree').addEventListener('click', hideTree)
-
 function hideTreeThree() {
-    console.log('clicked')
     document.getElementById('treeThree').style.display = 'none'
 }
 
-document.getElementById('treeFour').addEventListener('click', hideTree)
-
 function hideTreeFour() {
-    console.log('clicked')
     document.getElementById('treeFour').style.display = 'none'
 }
 
-document.getElementById('treeFive').addEventListener('click', hideTree)
-
 function hideTreeFive() {
-    console.log('clicked')
     document.getElementById('treeFive').style.display = 'none'
+}
+
+function hideTreeSix() {
+    document.getElementById('treeSix').style.display = 'none'
+}
+
+
+function hideTreeSeven() {
+    document.getElementById('treeSeven').style.display = 'none'
+}
+
+function hideTreeEight() {
+    document.getElementById('treeEight').style.display = 'none'
+}
+
+function hideTreeNine() {
+    document.getElementById('treeNine').style.display = 'none'
+}
+
+function hideTreeTen() {
+    document.getElementById('treeTen').style.display = 'none'
+}
+
+function hideTreeEleven() {
+    document.getElementById('treeEleven').style.display = 'none'
+}
+
+function hideTreeTwelve() {
+    document.getElementById('treeTwelve').style.display = 'none'
+}
+
+function hideTreeThirteen() {
+    document.getElementById('treeThirteen').style.display = 'none'
+}
+
+function hideTreeFourteen() {
+    document.getElementById('treeFourteen').style.display = 'none'
+}
+
+function hideTreeFifteen() {
+    document.getElementById('treeFifteen').style.display = 'none'
 }
