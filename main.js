e779486944e08d2f5656ec7d9b880adfd4b70015
Izzy <izzycalabig@hotmail.com>
2022-08-29 21:40:35 -0700

    Use visibility: hidden to make the trees hide

--- main.js
+++ main.js
@@ -2,83 +2,22 @@ document.getElementById('treeOne').addEventListener('click', hideTreeOne)
 document.getElementById('treeTwo').addEventListener('click', hideTreeTwo)
 document.getElementById('treeThree').addEventListener('click', hideTreeThree)
 document.getElementById('treeFour').addEventListener('click', hideTreeFour)
-document.getElementById('treeFive').addEventListener('click', hideTreeFive)
-document.getElementById('treeSix').addEventListener('click', hideTreeSix)
-document.getElementById('treeSeven').addEventListener('click', hideTreeSeven)
-document.getElementById('treeEight').addEventListener('click', hideTreeEight)
-document.getElementById('treeNine').addEventListener('click', hideTreeNine)
-document.getElementById('treeTen').addEventListener('click', hideTreeTen)
-document.getElementById('treeEleven').addEventListener('click', hideTreeEleven)
-document.getElementById('treeTwelve').addEventListener('click', hideTreeTwelve)
-document.getElementById('treeThirteen').addEventListener('click', hideTreeThirteen)
-document.getElementById('treeFourteen').addEventListener('click', hideTreeFourteen)
-document.getElementById('treeFifteen').addEventListener('click', hideTreeFifteen)
-document.getElementById('treeSixteen').addEventListener('click', hideTreeSixteen)
 
 // Separated by individual trees
 
 function hideTreeOne() {
-    document.getElementById('treeOne').style.display = 'none'
+    document.getElementById('treeOne').style.visibility = 'hidden'
 }
 
 function hideTreeTwo() {
-    document.getElementById('treeTwo').style.display = 'none'
+    document.getElementById('treeTwo').style.visibility = 'hidden'
 }
 
 function hideTreeThree() {
-    document.getElementById('treeThree').style.display = 'none'
+    document.getElementById('treeThree').style.visibility = 'hidden'
 }
 
 function hideTreeFour() {
-    document.getElementById('treeFour').style.display = 'none'
-}
-
-function hideTreeFive() {
-    document.getElementById('treeFive').style.display = 'none'
-}
-
-function hideTreeSix() {
-    document.getElementById('treeSix').style.display = 'none'
-}
-
-
-function hideTreeSeven() {
-    document.getElementById('treeSeven').style.display = 'none'
-}
-
-function hideTreeEight() {
-    document.getElementById('treeEight').style.display = 'none'
-}
-
-function hideTreeNine() {
-    document.getElementById('treeNine').style.display = 'none'
-}
-
-function hideTreeTen() {
-    document.getElementById('treeTen').style.display = 'none'
-}
-
-function hideTreeEleven() {
-    document.getElementById('treeEleven').style.display = 'none'
-}
-
-function hideTreeTwelve() {
-    document.getElementById('treeTwelve').style.display = 'none'
-}
-
-function hideTreeThirteen() {
-    document.getElementById('treeThirteen').style.display = 'none'
-}
-
-function hideTreeFourteen() {
-    document.getElementById('treeFourteen').style.display = 'none'
-}
-
-function hideTreeFifteen() {
-    document.getElementById('treeFifteen').style.display = 'none'
-}
-
-function hideTreeSixteen() {
-    document.getElementById('treeSixteen').style.display = 'none'
+    document.getElementById('treeFour').style.visibility = 'hidden'
 }
 
