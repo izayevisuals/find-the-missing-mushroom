aae049cf73525ba20aadf48df0c47a7c602df8cb
Izzy <izzycalabig@hotmail.com>
2022-09-06 18:52:02 -0700

    Added some buttons

--- main.js
+++ main.js
@@ -1,97 +1,14 @@
-// Click on a tree for it to hide
+// const musicIcon = document.getElementById('musicOn')
 
-document.getElementById('treeOne').addEventListener('click', hideTreeOne)
-document.getElementById('treeTwo').addEventListener('click', hideTreeTwo)
-document.getElementById('treeThree').addEventListener('click', hideTreeThree)
-document.getElementById('treeFour').addEventListener('click', hideTreeFour)
-document.getElementById('treeFive').addEventListener('click', hideTreeFive)
-document.getElementById('treeSix').addEventListener('click', hideTreeSix)
-document.getElementById('treeSeven').addEventListener('click', hideTreeSeven)
-// document.getElementById('treeSix').addEventListener('click', hideTreeSix)
-// document.getElementById('treeSeven').addEventListener('click', hideTreeSeven)
-// document.getElementById('treeEight').addEventListener('click', hideTreeEight)
-// document.getElementById('treeNine').addEventListener('click', hideTreeNine)
-// document.getElementById('treeTen').addEventListener('click', hideTreeTen)
-// document.getElementById('treeEleven').addEventListener('click', hideTreeEleven)
-// document.getElementById('treeTwelve').addEventListener('click', hideTreeTwelve)
-// document.getElementById('treeThirteen').addEventListener('click', hideTreeThirteen)
-// document.getElementById('treeFourteen').addEventListener('click', hideTreeFourteen)
-// document.getElementById('treeFifteen').addEventListener('click', hideTreeFifteen)
-// document.getElementById('treeSixteen').addEventListener('click', hideTreeSixteen)
+// let toggle = true;
 
-// Separated by individual trees
-
-function hideTreeOne() {
-    document.getElementById('treeOne').style.visibility = 'hidden'
-}
-
-function hideTreeTwo() {
-    document.getElementById('treeTwo').style.visibility = 'hidden'
-}
-
-function hideTreeThree() {
-    document.getElementById('treeThree').style.visibility = 'hidden'
-    document.querySelector('.messageWon').style.display = 'block'
-}
-
-function hideTreeFour() {
-    document.getElementById('treeFour').style.visibility = 'hidden'
-}
-
-function hideTreeFive() {
-    document.getElementById('treeFive').style.visibility = 'hidden'
-}
-
-function hideTreeSix() {
-    document.getElementById('treeSix').style.visibility = 'hidden'
-}
-
-function hideTreeSeven() {
-    document.getElementById('treeSeven').style.visibility = 'hidden'
-}
-
-// function hideTreeSix() {
-//     document.getElementById('treeSix').style.visibility = 'hidden'
-// }
-
-
-// function hideTreeSeven() {
-//     document.getElementById('treeSeven').style.visibility = 'hidden'
-// }
-
-// function hideTreeEight() {
-//     document.getElementById('treeEight').style.visibility = 'hidden'
-// }
-
-// function hideTreeNine() {
-//     document.getElementById('treeNine').style.visibility = 'hidden'
-// }
-
-// function hideTreeTen() {
-//     document.getElementById('treeTen').style.visibility = 'hidden'
-// }
-
-// function hideTreeEleven() {
-//     document.getElementById('treeEleven').style.visibility = 'hidden'
-// }
-
-// function hideTreeTwelve() {
-//     document.getElementById('treeTwelve').style.visibility = 'hidden'
-// }
-
-// function hideTreeThirteen() {
-//     document.getElementById('treeThirteen').style.visibility = 'hidden'
-// }
-
-// function hideTreeFourteen() {
-//     document.getElementById('treeFourteen').style.visibility = 'hidden'
-// }
-
-// function hideTreeFifteen() {
-//     document.getElementById('treeFifteen').style.visibility = 'hidden'
-// }
-
-// function hideTreeSixteen() {
-//     document.getElementById('treeSixteen').style.visibility = 'hidden'
-// }
-
+// musicIcon.addEventListener('click', function(){
+//     toggle = !toggle;
+//     if(toggle){
+//         musicIcon.src = 'music-on.png'
+//         mu
+//     }else{
+//         musicIcon.src = 'music-off.png'
+//     }
+    
+// })
